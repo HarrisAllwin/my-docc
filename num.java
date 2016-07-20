8b927eb139eef8e1341dce9205d7f8fde06ef39c
HarrisAllwin <harrisallwin25@gmail.com>
2016-07-20 15:40:45 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class num{
+	public static void main(String args[])
+	{
+	int a;
+	Scanner in=new Scanner(System.in);
+	a=in.nextInt();
+	if(a<0)
+	{
+	System.out.println("negative");
+	}
+	else{
+	System.out.println("positive");
+	}}
+
+}
